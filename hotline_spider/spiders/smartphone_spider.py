c554ec807a20c51ecca3a9bffd4d7ed4e34236d4
MaxVinohradov <vinogradov.max97@gmail.com>
2019-07-28 23:30:35 +0300

    feat(spider): add loader of time-series csv

--- hotline_spider/spiders/smartphone_spider.py
+++ hotline_spider/spiders/smartphone_spider.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+import pprint
+import random
+from urllib.request import urlopen
+import csv
+import datetime
+from io import StringIO
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+
+pp = pprint.PrettyPrinter(width=41, compact=True)
 
 
 class SmartphoneSpiderSpider(CrawlSpider):
@@ -16,10 +24,33 @@ class SmartphoneSpiderSpider(CrawlSpider):
     def parse_item(self, response):
         product = {}
         features = {}
+        product['features'] = features
         product['card_title'] = str(response.xpath('//h1[@datatype="card-title"]/text()').get()).strip()
+        product['card_id'] = str(response.css('span[data-card-id]::attr(data-card-id)').get()).strip()
         for index, feature in enumerate(response.xpath('//table[@class="seo-table"]/tr')):
             key = feature.xpath('td/text()').get()
             value = feature.xpath('td/span/a/text()').get() or feature.xpath('td/p/text()').get()
             if (key and value):
                 features[key.strip().replace(':', '')] = value.strip()
+        product['time_series'] = SmartphoneSpiderSpider.load_csv(product['card_id'])
+        pp.pprint(product)
         yield product
+
+    @staticmethod
+    def load_csv(card_id):
+        card_id_str = str(card_id)
+        rnd = random.uniform(0, 1)
+        csv_url = f'https://hotline.ua/temp/charts/{card_id_str[:-2]}/{card_id_str[-2:]}complex.csv?rnd={rnd}'
+        data = urlopen(csv_url).read().decode('ascii', 'ignore')
+        datafile = StringIO(data)
+        csv_reader = csv.reader(datafile, delimiter=';')
+        data = []
+        for row in csv_reader:
+            date, price, quant, popul = row
+            data.append({  
+                'date': datetime.datetime.strptime(date, '%d.%m.%Y'),
+                'price': float(price),
+                'quant': float(quant),
+                'popul': float(popul),
+            })
+        return data
